9c6e6961f2f4b98d08147e9853a8dc7750d5d8fb
ancimpek <anci.mpek@gmail.com>
2021-09-10 15:11:00 +0200

    Modified Main.java file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number 1");
+        int num1 = s.nextInt();
 
+        System.out.println("Enter the number 2");
+        int num2 = s.nextInt();
+        s.close();
+
+        int result = num1 + num2;
+        System.out.printf("%d + %d = %d", num1, num2, result);
     }
 }
